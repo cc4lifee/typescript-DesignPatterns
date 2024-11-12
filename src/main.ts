99b58cc0cb40ee773ae18e80f1744f2a21dd9f47
Cristhian Camacho <cristhian.camacho@gulfstream.com>
2024-11-12 09:19:00 -0800

    first commit x2

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -17,4 +16,3 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
